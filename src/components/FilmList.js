84f1cbed386a9a1f832b53f7c4bd9907f8f10ccb
Connor <connorjrobertson@hotmail.co.uk>
2021-07-26 17:06:54 +0100

    Monday hw complete

---
+++ src/components/FilmList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Film from './Film.js';
+
+const FilmList = ({films}) => {
+
+    const filmNodes = films.map(film =>{
+        return(
+            <Film name={film.name} url={film.url} key={film.id}/> 
+        )
+    });
+
+    return(
+        <>
+            {filmNodes}
+        </>
+    )
+};
+
+export default FilmList;
